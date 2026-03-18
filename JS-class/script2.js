33b59c1a578f3d62211786dc48016f3bfd39f3d9
alokpadal-gif <alokpadal45@gmail.com>
2026-03-18 10:33:47 +0545

    JS

---
+++ JS-class/script2.js
@@ -0,0 +1,19 @@
+
+let a = 50;
+let b = 75;
+let c = 100;
+let x = 75;
+
+
+if (x < 50) {
+    alert(`x = ${x} is less than 50`);
+} else if (x < 100) {
+    alert(`x = ${x} is less than 100 but greater than or equal to 50`);
+} else {
+    alert(`x = ${x} is 100 or more`);
+}
+
+alert(`Values:
+a = ${a}, a + 5 = ${a + 5}
+b = ${b}, b + 5 = ${b + 5}
+c = ${c}, c + 5 = ${c + 5}`);
